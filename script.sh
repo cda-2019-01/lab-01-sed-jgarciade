c859902d67b12993ae931ce2986ece8e7f16cd89
jose garcia <jgarciade@unal.edu.co>
2019-05-16 00:38:36 +0000

    adding docs comments to script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,29 @@
 # Escriba su código aquí
 file=data.csv
-sed 's/\([0-9]*\)\/\([0-9]*\)\/\([0-9]*\);/20\3-\2-\1;/' $file | sed 's/;[^A,B,C,a,b,c]{1};/;\\N;/'| sed 's/;;/;\\N;/' | sed 's/;[a,b,c];/\U&/g' | sed 's/;N/;\\N/' | sed 's/;\([0-9]*\),\([0-9]\)/;\1\.\2/' | sed 's/;\([0-9]*\);\([0-9]*\);[0-9]*/;\1;\2/' | sed '/\\N/d' | sed 's/;/,/g'> results.csv
+
+# Changes '\' to '-'
+sed 's/\([0-9]*\)\/\([0-9]*\)\/\([0-9]*\);/20\3-\2-\1;/' $file | \
+
+# Fills with null any value in the first row that is not in {A,B,C,a,b,c}
+sed 's/;[^A,B,C,a,b,c]{1};/;\\N;/'| \
+
+# Fills wiht null empty fields
+sed 's/;;/;\\N;/' | \
+
+# Cast from lower to upper case
+sed 's/;[a,b,c];/\U&/g' | \
+
+# Changes the char 'N' to null
+sed 's/;N/;\\N/' | \
+
+# Changes comma for point as decimal separator for the last row
+sed 's/;\([0-9]*\),\([0-9]\)/;\1\.\2/' | \
+
+# Deletes rows with 5 columns discarding the last one
+sed 's/;\([0-9]*\);\([0-9]*\);[0-9]*/;\1;\2/' | \
+
+# Deletes fields with nulls
+sed '/\\N/d' | \
+
+# Changes comma for semicolon as column separator
+sed 's/;/,/g'> results.csv
